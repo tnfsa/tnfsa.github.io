062742a85c4f0d5af5d2c4cdf49bf86d9d42ed03
alias722 <wei@sivir.pw>
2021-05-06 02:05:34 +0800

    add carousil in main page

--- src/components/signinblock.js
+++ src/components/signinblock.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
 import GoogleOauth from "./googleOauth";
+import {
+    Container,
+    Row,
+    Col,
+} from 'react-bootstrap'
 
 class Signinblock extends React.Component{
     render(){
@@ -11,36 +16,37 @@ class Signinblock extends React.Component{
         }
         return(
             <React.Fragment>
-                <lebal style={{width:'15%',float:'left'}}>&nbsp;</lebal>
-                <div>
-                <form className={"signInBlock"} style={{width: '70%',display:'inline-block',float:'left'}}>
-                    <h3>Sign In</h3>
-                    <div className="form-group">
-                        <label>Email address</label>
-                        <input id="userInputEmail" type="email" className="form-control" placeholder="Enter email" />
-                    </div>
+                <Container>
+                    <form className={"signInBlock"}>
+                        <h3 style={{textAlign: 'center'}}>Sign In</h3>
+                        <div className="form-group">
+                            <label>Email address</label>
+                            <input id="userInputEmail" type="email" className="form-control" placeholder="Enter email" />
+                        </div>
 
-                    <div className="form-group">
-                        <label>Password</label>
-                        <input id="userInputPasswd" type="password" className="form-control" placeholder="Enter password" />
-                    </div>
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input id="userInputPasswd" type="password" className="form-control" placeholder="Enter password" />
+                        </div>
 
-                    {/*<div className="form-group">
+                        {/*<div className="form-group">
                         <div className="custom-control custom-checkbox">
                             <input type="checkbox" className="custom-control-input" id="customCheck1" />
                             <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
                         </div>
                     </div>*/}
-
-                    <button type="submit" className="btn btn-primary btn-block" onClick={Send}>Submit</button>
-                </form>
-                <div className={"otherLoginMethod"} >
-                    <lebal style={{width:'37%',float:'left'}}>&nbsp;</lebal>
-                    <GoogleOauth />
-                    <br />
-                    <label style={{width:'60%',float:'right'}}>僅限本校google帳號</label>
-                </div>
-                </div>
+                        <Row>
+                            <Col></Col>
+                            <Col></Col>
+                            <Col><button type="submit" className="btn btn-primary btn-block" onClick={Send}>Submit</button></Col>
+                        </Row>
+                        <hr />
+                        <label>另一種登入方式？</label>
+                        <div className={"otherLoginMethod"} >
+                            <GoogleOauth />
+                        </div>
+                    </form>
+                </Container>
             </React.Fragment>
         )
     }
--- src/page/main.js
+++ src/page/main.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 import Navibar from "../components/navigationbar";
+import Carousels from '../components/carousel'
+
 class Homepage extends React.Component{
 	render(){
 		return(
 			<React.Fragment>
 				<Navibar/>
+				<Carousels />
 			</React.Fragment>
 		)
 	}
