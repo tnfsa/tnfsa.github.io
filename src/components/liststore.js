8a1d5a9a6a91334b46ac1cb0dee735021ecc3524
Hung-Wei Cheng <maxchengboy@gmail.com>
2021-05-19 10:15:21 +0800

    list store good

--- src/components/liststore.js
+++ src/components/liststore.js
@@ -1,17 +1,16 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 import config from '../config.json'
-import store from "../page/sells/store";
+
 import {
     Card,
-    Button,
-    Row
+    Button
 } from 'react-bootstrap'
 
+function ListStore(){
+    const [data,setData] = useState([])
 
-class Liststore extends React.Component{
-    render(){
-        let listFile = []
+    const getData = ()=>{
         const url = config['baseURL'] + 'stores'
         fetch(url,{
             method: 'GET'
@@ -30,25 +29,73 @@ class Liststore extends React.Component{
             )
             window.location.replace('#/login')
             return
-        }).then(storeData =>{
-            listFile = storeData
+        }).then(myJson =>{
+            console.log(myJson)
+            setData(myJson)
         })
+    }
+    useEffect(()=>{
+        getData()
+    },[])
+    return(
+        <div className="ListStore">
+            {
+                data && data.length>0 && data.map((item)=>
+                    <Card>
+                        <Card.Img variant="top" src={item.picUrl} />
+                        <Card.Body style={{display:"flex"}}>
+                            <div>
+                                <Card.Title>{item.name}</Card.Title>
+                                <Card.Text>{item.description}</Card.Text>
+                            </div>
+                            <div style={{marginLeft:"auto"}}>
+                                <Button variant="primary" href={config["project"]+'#/order/'+item.id}>立即前往</Button>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                )
+            }
+        </div>
+    )
+}
+
+
+/*class Liststore extends React.Component{
+    render(){
+
+        const getData = ()=>{
+            const url = config['baseURL'] + 'stores'
+            fetch(url,{
+                method: 'GET'
+            }).then(response => {
+                if (response.ok) {
+                    return response.json()
+                }
+                return response.text().then(res => {
+                    throw new Error(res)
+                })
+            }).catch((error) => {
+                console.log(error.message)
+                let response = JSON.parse(error.message)
+                window.alert(
+                    `${response.message}\n與伺服器連線錯誤，請再試一次\n如果問題無法解決，請聯絡管理員`
+                )
+                window.location.replace('#/login')
+                return
+            }).then(storeData =>{
+                console.log(storeData)
+                setData(storeData)
+            })
+        }
 
         const ListItem = listFile.map((item)=>
-            <Card id={item.toString()}>
-                <Card.Img variant="top" src="holder.js/100px180" />
-                <Card.Body>
-                    <Card.Title>{item.title}</Card.Title>
-                    <Card.Text>{item.text}</Card.Text>
-                    <Button variant="primary" target="_blank" href={item.link}>前往察看</Button>
-                </Card.Body>
-            </Card>
+            <p>Hello</p>
         )
 
         return(
             {ListItem}
         )
     }
-}
+}*/
 
-export default Liststore
+export default ListStore
