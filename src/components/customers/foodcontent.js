90626a6bae7392c5600c97c34f247d2fef679238
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2021-06-16 14:14:03 +0800

    Update Alert and fix transaction feature

--- src/components/customers/foodcontent.js
+++ src/components/customers/foodcontent.js
@@ -1,32 +1,36 @@
-import React, {useEffect,useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import config from '../../config.json'
 import {Container, Row, Col, Figure, Button} from 'react-bootstrap'
+import {useHistory} from 'react-router-dom'
 import Cookies from 'universal-cookie'
+import Swal from 'sweetalert2'
 
-export default function FoodContent(props){
-    const [title,setTitle] = useState('')
-    const [picture,setPicture] = useState('')
-    const [price,setPrice] = useState('')
-    const [description,setDescription] = useState('')
+export default function FoodContent(props) {
+    const [title, setTitle] = useState('')
+    const [picture, setPicture] = useState('')
+    const [price, setPrice] = useState('')
+    const [description, setDescription] = useState('')
+    const history = useHistory()
     const URL = config['baseURL'] + 'stores/' + props.store + '/products/' + props.product;
-    async function getData(){
-        fetch(URL,{
+
+    async function getData() {
+        fetch(URL, {
             method: 'GET',
-            headers:{
+            headers: {
                 "Accept": "application/json",
             }
-        }).then(response=>{
-            if(response.ok){
+        }).then(response => {
+            if (response.ok) {
                 return response.json()
             }
-            return response.text().then(err =>{
+            return response.text().then(err => {
                 throw new Error(err)
             })
-        }).catch(error=>{
+        }).catch(error => {
             console.log(error.message)
             let response = JSON.parse(error.message)
             window.alert(`${response.message}\n與伺服器連線錯誤，請再試一次\n如果問題無法解決，請聯絡管理員`)
-        }).then(response=>{
+        }).then(response => {
             console.log(response)
             setPrice(response['price'])
             setDescription(response['description'])
@@ -34,11 +38,13 @@ export default function FoodContent(props){
             setPicture(response['image'])
         })
     }
+
     const cookies = new Cookies()
     const allcookies = cookies.getAll()
-    function Send(){
+
+    function Send() {
         const confirmText =
-`請確認您的訂購資訊
+            `請確認您的訂購資訊
 名稱：${title}
 售價：${price}
 其他建議：(功能未開放)
@@ -47,12 +53,20 @@ export default function FoodContent(props){
             //window.alert('Ok')
             // send transaction
             const url = config['baseURL'] + 'transactions'
-            fetch(url,{
+            fetch(url, {
                 method: 'POST',
-                "Authorization": `Bearer ${allcookies['session']}`,
-                headers:{
+                body: JSON.stringify({
+                    "name": title,
+                    "qty": 1,
+                    "store_id": props.store,
+                    "product_id": props.product,
+                    "comment": "",
+                    "options": {}
+                }),
+                headers: {
                     "Accept": "application/json",
-                    "Content-Type": "application/json"
+                    "Content-Type": "application/json",
+                    "Authorization2": `Bearer ${allcookies['session']}`
                 }
             }).then(response => {
                 if (response.ok) {
@@ -61,30 +75,41 @@ export default function FoodContent(props){
                 return response.text().then(res => {
                     throw new Error(res)
                 })
+            }).then(() => {
+                // Transaction Made
+                window.alert('訂購成功!')
+                cookies.set('alert', '感謝您利用本系統訂購產品\n請記得於選取時間取餐，謝謝', {path: '/'})
+                history.push('/')
             }).catch((error) => {
-                console.log(error.message)
+                // Transaction Error
+                console.log(error)
                 let response = JSON.parse(error.message)
-                window.alert(`${response.message}\n與伺服器連線錯誤，請再試一次\n如果問題無法解決，請聯絡管理員`)
-            }).then(
-                //good
-                cookies.set('alert','感謝您利用本系統訂購產品\n請記得於選取時間取餐，謝謝',{path:'/'}),
-                document.location.replace('/')
-            )
+                Swal.fire({
+                    title: '錯誤!',
+                    text: `${response.message}\n與伺服器連線錯誤，請再試一次\n如果問題無法解決，請聯絡管理員`,
+                    icon: 'error',
+                    confirmButtonText: 'Ok'
+                }).then(() => history.push('/'))
+            })
         } else {
             // fallback to center
-            cookies.set('alert','訂單未成立',{path:'/'})
-            document.location.replace('/')
+            Swal.fire({
+                title: '錯誤!',
+                text: `訂單未成立`,
+                icon: 'error',
+                confirmButtonText: 'Ok'
+            }).then(() => history.push('/'))
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         getData()
-    },[])
+    })
 
-    return(
+    return (
         <React.Fragment>
             <Container>
-                <br />
+                <br/>
                 <h1><Row><Col>餐點名稱：</Col><Col>{title}</Col></Row></h1>
                 <Figure>
                     <Figure.Image
@@ -98,8 +123,8 @@ export default function FoodContent(props){
                     </Figure.Caption>
                 </Figure>
                 <h2><Row><Col>建議售價：</Col><Col>{price}</Col></Row></h2>
-                <div style={{display:"flex"}}>
-                    <div style={{marginLeft:"auto"}}>
+                <div style={{display: "flex"}}>
+                    <div style={{marginLeft: "auto"}}>
                         <Button variant="primary" onClick={Send}>訂購</Button>
                     </div>
                 </div>
