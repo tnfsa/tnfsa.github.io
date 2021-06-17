3c7d4d312a2f35596a1b13a6ff64541cf8b5eab3
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2021-06-17 22:12:51 +0800

    Update Customer Service

---
+++ src/page/sells/CustomerServiceComponent/DiskQuota.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {Card} from "react-bootstrap";
+
+class DiskQuota extends React.Component {
+    render() {
+        return (
+            <Card className={["m-3", "p-3"]}>
+                <div hidden={!this.props.quota.exceed_quota}>
+                    儲存空間已滿
+                </div>
+                <div>
+                    儲存空間剩餘 {(this.props.quota.remain / 1024000).toFixed(2)} Mb / {this.props.quota.total} Mb
+                    (已用{(((this.props.quota.total-(this.props.quota.remain / 1024000))/this.props.quota.total)*100).toFixed(2)}%)
+                </div>
+
+            </Card>
+        )
+    }
+
+}
+
+export default DiskQuota
--- src/page/sells/service.js
+++ src/page/sells/service.js
@@ -1,18 +1,54 @@
 import React from 'react'
-import {Spinner} from "react-bootstrap";
+import {Col, Spinner, Row} from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faHeadphones } from '@fortawesome/free-solid-svg-icons'
+import {faHeadphones} from '@fortawesome/free-solid-svg-icons'
+import DiskQuota from "./CustomerServiceComponent/DiskQuota";
+import Cookies from "universal-cookie/lib";
+import {API} from "../../helpers/API";
 
 class CustomerService extends React.Component {
-    componentDidMount() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            quota: {},
+            loading: false
+        }
+    }
+
+    componentDidMount(props) {
         window.scrollTo({top: 0, behavior: 'smooth'})
+        const cookies = new Cookies()
+        const api = new API()
+        this.setState({
+            loading: true
+        })
+        api.call('/stores/:store/disk_quota', {
+            method: "GET",
+            params: {
+                store: cookies.get('storeId')
+            }
+        }, (quota) => {
+            console.log(quota.remain)
+            this.setState({
+                quota,
+                loading: false
+            });
+        })
     }
 
     render() {
         return (
             <React.Fragment>
                 <h1 style={{textAlign: 'center'}}>客服中心 <FontAwesomeIcon icon={faHeadphones}/></h1>
-                <Spinner animation={"border"}/>
+                <Row>
+                    <Col>
+                        <center><Spinner animation={"border"} hidden={!this.state.loading}/></center>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col><DiskQuota quota={this.state.quota}/></Col>
+
+                </Row>
             </React.Fragment>
         )
     }
