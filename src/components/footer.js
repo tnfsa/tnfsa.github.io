20884b1985609e0b2ac5f178021d393e3ad32c8a
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2021-06-17 11:07:07 +0800

    Move version to bottom bar

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import {Col, Container, Row} from "react-bootstrap";
 import {Link} from "react-router-dom";
+import config from "../config.json";
 
 class Footer extends React.Component{
     render(){
         return(
             <React.Fragment>
                 <br />
-                <div className={"p-3 mb-2 bg-dark text-white"}>
+                <div className={"p-3 mb-2 bg-dark text-white "}>
                     <Container>
                         <br />
                         <Row>
@@ -21,6 +22,9 @@ class Footer extends React.Component{
                                 <a href='https://sivir.pw' rel="noreferrer noopener" target='_blank'>Alias722</a>、
                                 <a href='https://hsuan.app/' rel="noreferrer noopener" target='_blank'>Hsuan1117</a>、
                                 <a href='https://neodoggy.github.io/' rel="noreferrer noopener" target='_blank'>Neodoggy</a>
+                                <br/>
+                                <br/>
+                                <span>version: {config['version']}</span>
                             </Col>
                         </Row>
                     </Container>
