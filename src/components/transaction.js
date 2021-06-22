6fbcff4db1f73de24deea9303f44c321be66e164
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2021-06-22 11:30:14 +0800

    add rating system

--- src/components/transaction.js
+++ src/components/transaction.js
@@ -1,18 +1,25 @@
 import React, {useEffect, useState} from 'react'
 import Cookies from 'universal-cookie'
-import {Card, Container} from 'react-bootstrap'
+import {Card, Col, Container, Row} from 'react-bootstrap'
+import {Rating} from "@material-ui/lab";
+import {Grid, IconButton, LinearProgress} from "@material-ui/core";
+import {API} from "../helpers/API";
+import {Delete} from "@material-ui/icons";
 
 
-export default function Transaction(){
-    const [transaction,setTransaction] = useState([])
+export default function Transaction() {
+    const [transaction, setTransaction] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const api = new API();
 
-    async function Update(){
+    async function Update() {
         const cookies = new Cookies()
         const allcookies = cookies.getAll()
         const fetchURL = process.env.REACT_APP_API_ENDPOINT + '/transactions'
-        fetch(fetchURL,{
+        setLoading(true);
+        fetch(fetchURL, {
             method: 'GET',
-            headers:{
+            headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
                 'Authorization': `Bearer ${allcookies['session']}`
@@ -34,29 +41,72 @@ export default function Transaction(){
         }).then(r => {
             console.log(r)
             setTransaction(r)
+            setLoading(false)
         })
     }
 
-    useEffect(()=>{
-        Update()
-    },[])
+    function setNewRate(val, id) {
+        setLoading(true)
+        api.call('/transactions/:transaction/rate', {
+            method: 'POST',
+            params: {
+                transaction: id
+            },
+            body: {
+                rate: val
+            }
+        }, (r) => {
+            Update()
+        })
+    }
 
-    return(
+    useEffect(() => {
+        Update()
+    }, [])
+
+    return (
         <Container>
+            <LinearProgress hidden={!loading}/>
             <div className={"History"}>
                 {
-                    transaction && transaction.length>0 && transaction.map((item)=>
-                        <Card>
-                            <Card.Img variant="top" src={item.picUrl} />
-                            <Card.Body style={{display:"flex"}}>
+                    transaction && transaction.length > 0 && transaction.map((item) =>
+                        <Row key={item.id}>
+                            <Col sm={12} md={6}>
+                                <Card>
+                                    <Card.Img variant="top" src={item.picUrl}/>
+                                    <Card.Body style={{display: "flex", flexDirection: "column",}}>
+                                        <Card.Title>商品名稱：{item.product.name}</Card.Title>
+                                        <Card.Title>金額：{item.total}</Card.Title>
+                                        <Card.Text>留言：{item.comment}</Card.Text>
+                                        <Card.Text>購買日期：{new Date(item.updated_at).toLocaleTimeString('zh-TW')}</Card.Text>
+                                        <Card.Text>訂單編號：<b>{item.id.substring(0, 8)}</b>{item.id.substr(8)}</Card.Text>
+                                        <Grid container spacing={1} justify="center" direction="row"
+                                              alignItems="center">
+                                            <Grid item>
+                                                <Rating name="rating" value={parseFloat(item.rating)}
+                                                        onChange={e => setNewRate(e.target.value, item.id)}/>
+                                            </Grid>
+                                            <Grid item>
+                                                <IconButton
+                                                    onClick={() => setNewRate(0, item.id)}
+                                                ><Delete size="small"/></IconButton>
+                                            </Grid>
+                                        </Grid>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col sm={12} md={6} style={{justifyContent: 'center', display: "flex"}}>
                                 <div>
-                                    <Card.Title>商品名稱：{item.product_id}</Card.Title>
-                                    <Card.Title>金額：{item.total}</Card.Title>
-                                    <Card.Text>留言：{item.comment}</Card.Text>
-                                    <Card.Text>購買日期：{item.updated_at}</Card.Text>
+                                    <span style={{
+                                        alignItems: "center",
+                                        fontSize: "5rem",
+                                        fontStyle: "bold",
+                                        display: "flex",
+                                        textAlign: "center"
+                                    }}>{item.id.substring(0, 8)}</span>
                                 </div>
-                            </Card.Body>
-                        </Card>
+                            </Col>
+                        </Row>
                     )
                 }
             </div>
