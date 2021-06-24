307ca46d7695b9ce4ccb32edd228b428e2d4f37f
alias722 <wei@sivir.pw>
2021-06-24 23:33:54 +0800

    timeout

--- src/page/sells/viewdetailedbooking.js
+++ src/page/sells/viewdetailedbooking.js
@@ -1,10 +1,9 @@
-import React,{useEffect,useState} from 'react'
-import {Card, Container, Spinner} from "react-bootstrap";
+import React,{useEffect, useState} from 'react'
+import {Card, Container, Spinner, Row, Col} from "react-bootstrap";
 import Cookies from 'universal-cookie'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faTimes, faCheck, faUserCheck} from '@fortawesome/free-solid-svg-icons'
 import {Button} from '@material-ui/core'
-
 
 export default function ViewDetailedBooking(props){
     const [loading,setLoading] = useState(true)
@@ -12,6 +11,7 @@ export default function ViewDetailedBooking(props){
     const [productName,setProductName] = useState('')
     const cookies = new Cookies()
     const allcookies = cookies.getAll()
+    const [changes,setChanges] = useState(0)
 
     const transactions = async ()=>{
         const url = process.env.REACT_APP_API_ENDPOINT + '/stores/' + allcookies['storeId'] + '/transactions'
@@ -68,15 +68,21 @@ export default function ViewDetailedBooking(props){
     }
     function finished(transaction_id){
         const url = process.env.REACT_APP_API_ENDPOINT + '/transactions/'+ transaction_id
-        sendStatus(url,'OK')
+        sendStatus(url,'OK').then(()=>{
+            setChanges(changes+1)
+        })
     }
     function preparing(transaction_id){
         const url = process.env.REACT_APP_API_ENDPOINT + '/transactions/'+ transaction_id
-        sendStatus(url,'PREPARE')
+        sendStatus(url,'PREPARE').then(()=>{
+            setChanges(changes+1)
+        })
     }
     function taken(transaction_id){
         const url = process.env.REACT_APP_API_ENDPOINT + '/transactions/'+ transaction_id
-        sendStatus(url,'DONE')
+        sendStatus(url,'DONE').then(()=>{
+            setChanges(changes+1)
+        })
     }
 
     const sendStatus = async (url,status)=>{
@@ -108,8 +114,9 @@ export default function ViewDetailedBooking(props){
     }
 
     useEffect(()=>{
-        return getInfo()
-    })
+        getInfo()
+        // eslint-disable-next-line
+    },[changes])
 
     return(
         <React.Fragment>
@@ -122,31 +129,39 @@ export default function ViewDetailedBooking(props){
                     data ? data.map((item) =>
                         <Card key={item.id}>
                             <Card.Body style={{display: "flex"}}>
-                                <div>
-                                    <Card.Title>交易編號：{item.id}</Card.Title>
-                                    <Card.Text>備註：{item.comment}</Card.Text>
-                                </div>
-                                <div style={{marginLeft: "auto"}}>
-                                    <Button
-                                        variant="contained"
-                                        color={item.status === 'PREPARE'? 'primary':''}
-                                        onClick={()=>{preparing(item.id)}}
-                                    >
-                                        <FontAwesomeIcon icon={faTimes}/>
-                                    </Button>
-                                    <Button
-                                        variant="contained"
-                                        color={item.status === 'OK'? 'primary':''}
-                                        onClick={()=>{finished(item.id)}}>
-                                        <FontAwesomeIcon icon={faCheck}/>
-                                    </Button>
-                                    <Button
-                                        variant="contained"
-                                        color={item.status === 'DONE'? 'primary':''}
-                                        onClick={()=>{taken(item.id)}}>
-                                        <FontAwesomeIcon icon={faUserCheck}/>
-                                    </Button>
-                                </div>
+                                <Row>
+                                    <Col xs={10}>
+                                        <Card.Title>交易編號：{item.id}</Card.Title>
+                                        <Card.Text>備註：{item.comment}</Card.Text>
+                                    </Col>
+                                    <Col>
+                                        <Row>
+                                            <Button
+                                                variant="contained"
+                                                color={item.status === 'PREPARE'? 'primary':''}
+                                                onClick={()=>{preparing(item.id)}}
+                                            >
+                                                <FontAwesomeIcon icon={faTimes}/>
+                                            </Button>
+                                        </Row>
+                                        <Row>
+                                            <Button
+                                                variant="contained"
+                                                color={item.status === 'OK'? 'primary':''}
+                                                onClick={()=>{finished(item.id)}}>
+                                                <FontAwesomeIcon icon={faCheck}/>
+                                            </Button>
+                                        </Row>
+                                        <Row>
+                                            <Button
+                                                variant="contained"
+                                                color={item.status === 'DONE'? 'primary':''}
+                                                onClick={()=>{taken(item.id)}}>
+                                                <FontAwesomeIcon icon={faUserCheck}/>
+                                            </Button>
+                                        </Row>
+                                    </Col>
+                                </Row>
                             </Card.Body>
                         </Card>
                     ) : <React.Fragment><br/><h2 style={{textAlign: 'center'}}>查無資料</h2></React.Fragment>
